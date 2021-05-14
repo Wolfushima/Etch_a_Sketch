64ef3dab7c886f91c3813e3a81c09af16def78df
Adolfo Herrera <valherdolf@gmail.com>
2021-05-14 00:08:54 -0400

    Added a button that clears the grid backgroundcolor, and added an eventListener to the divElement that changes it's background color to black

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const gridContent = document.querySelector(".grid-content");
+const clearButton = document.querySelector(".clear-button");
+clearButton.addEventListener("click", () => {
+    const arrayGridContent = Array.from(gridContent.childNodes);
+    arrayGridContent.forEach(element => {
+        element.style.backgroundColor = "white";
+    });
+});
 
 for (let i = 0; i < 16 * 16; i++) {
     const divElement = document.createElement("div");
     divElement.classList.add("div-element");
     gridContent.appendChild(divElement);
+    divElement.addEventListener("mousemove", () => {
+        divElement.style.backgroundColor = "rgb(0,0,10)";
+    })
 }
